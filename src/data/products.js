4bb3248248730f5809556b99e5ca928490e5faf7
thearaa240-collab <thearaa240@gmail.com>
2026-03-03 11:03:18 +0700

    commit

--- src/data/products.js
+++ src/data/products.js
@@ -4,6 +4,7 @@ const products = [
   { id: 1, name: "Running Shoes", price: 89.99 },
   { id: 2, name: "Basketball Sneakers", price: 120.0 },
   { id: 3, name: "Casual Loafers", price: 59.5 },
+  { id: 4, name: "Dress sneakers", price: 59.5 },
 ];
 
 export default products;
